441e3930b6b5946a26b5f1405d228ef1c7354046
Gabriel Sakoda <otanigabriel0@gmail.com>
2025-02-27 01:43:34 -0300

    fix: fixing the question

--- test_json.py
+++ test_json.py
@@ -1,7 +1,7 @@
 from math_tutor import virtual_math_teacher
 import json
 
-entry_json = json.dumps({"pergunta": "Qual é a fórmula de bhaskara?"})
+entry_json = json.dumps({"pergunta": "Resolva a equação 2x+3=7"})
 
 answer_json = virtual_math_teacher(entry_json)
 
